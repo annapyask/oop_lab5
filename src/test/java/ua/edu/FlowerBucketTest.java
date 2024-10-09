e175997f6cbb29ed774a60ebba82b8835ab9171b
Anna Piaskovska <piaskovska.pn@ucu.edu.ua>
2024-10-09 14:21:43 +0300

    test class

---
+++ src/test/java/ua/edu/FlowerBucketTest.java
@@ -0,0 +1,36 @@
+package ua.edu;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import ua.edu.ucu.aps.flower.Flower;
+import ua.edu.ucu.aps.flower.FlowerBucket;
+import ua.edu.ucu.aps.flower.FlowerPack;
+
+import org.junit.jupiter.api.Assertions;
+
+import java.util.Random;
+
+public class FlowerBucketTest {
+    private static final Random RANDOM_GENERATOR = new Random();
+    private static final int MAX_QUANTITY = 1000;
+    private static final int MAX_PRICE = 100;
+
+    private FlowerBucket flowerBucket;
+
+    @BeforeEach
+    public void init() {
+        flowerBucket = new FlowerBucket();
+    }
+
+    @Test
+    public void testPrice() {
+        int price = RANDOM_GENERATOR.nextInt(MAX_PRICE);
+        int quantity = RANDOM_GENERATOR.nextInt(MAX_QUANTITY);
+        Flower flower = new Flower();
+        flower.setPrice(price);
+        FlowerPack flowerPack = new FlowerPack(flower, quantity);
+        flowerBucket.add(flowerPack);
+        Assertions.assertEquals(price * quantity, flowerBucket.getPrice());
+    }
+}
